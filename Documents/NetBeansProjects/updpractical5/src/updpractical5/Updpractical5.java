db41b56be61d4d1e88913a8ba0a20a8be31c4ebb
sarveshsolanke <sarveshsolanke@github.com>
2020-10-08 16:12:09 +0530

    initial commit

---
+++ Documents/NetBeansProjects/updpractical5/src/updpractical5/Updpractical5.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package updpractical5;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author HP PC
+ */
+public class Updpractical5 {
+
+   
+    public static void main(String[] args) throws SocketException, UnknownHostException, IOException {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("*****UDP CLIENT SIDE***********");
+        DatagramSocket ds=new DatagramSocket();
+        int i=6;
+        System.out.println("***menu***");
+        System.out.println("Addition of nums");
+        System.out.println("Subtraction of numsAddition of nums");
+        System.out.println("Multiplication of nums");
+        System.out.println("divition");
+        int ch=sc.nextInt();
+          System.out.println("enter two numbers");
+          int num1=sc.nextInt();
+       
+        int num2=sc.nextInt();
+        
+        
+        byte[] bch=String.valueOf(ch).getBytes();
+        byte[] bnum1=String.valueOf(num1).getBytes();
+        byte[] bnum2=String.valueOf(num2).getBytes();
+        InetAddress ia=InetAddress.getLocalHost();
+        DatagramPacket dp=new DatagramPacket(bch,bch.length,ia,9999);
+        DatagramPacket dp1n=new DatagramPacket(bnum1,bnum1.length,ia,9999);
+        DatagramPacket dp2n=new DatagramPacket(bnum2,bnum2.length,ia,9999);
+        ds.send(dp);
+          ds.send(dp1n);
+            ds.send(dp2n);
+       
+        
+        
+         byte[] b1=new byte[1024];
+         byte[] b2=new byte[1024];
+      //  InetAddress ia=InetAddress.getLocalHost();
+        DatagramPacket dp1=new DatagramPacket(b1,b1.length,ia,9999);
+        ds.receive(dp1);
+       DatagramPacket dpflag=new DatagramPacket(b2,b2.length,ia,9999);
+        ds.receive(dpflag);
+        String str=new String(dp1.getData());
+        
+        
+        String flagst=new String(dpflag.getData());
+           int nums=Integer.parseInt(flagst.trim());
+      //   System.out.println("flag="+flagst);
+           
+         System.out.println("Result Value from Server......");
+         System.out.println("  ");
+         if(nums==1){System.out.println(num1+"+"+num2+"="+str);}
+         else if(nums==2){System.out.println(num1+"-"+num2+"="+str);}
+         else if(nums==3){System.out.println(num1+"*"+num2+"="+str);}
+         else if(nums==4){System.out.println(num1+"/"+num2+"="+str);}
+         else{System.out.println("someting went wrong retatagramPacket ");}
+     //   System.out.println("num1"+"*"+"numm2"+"="+str);
+        
+    }
+    
+}
---
+++ Documents/NetBeansProjects/updpractical5/src/updpractical5/udpserver_conn_localhost.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package updpractical5;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+/**
+ *
+ * @author HP PC
+ */
+public class udpserver_conn_localhost {
+    
+    public static void main(String args[]) throws SocketException, IOException
+    {DatagramSocket ds=new DatagramSocket(9999);
+    byte[] b1ch= new byte[1024];
+      byte[] b1num1= new byte[1024];
+        byte[] b1num2= new byte[1024];
+        
+        DatagramPacket dpchoice=new DatagramPacket(b1ch,b1ch.length);
+        ds.receive(dpchoice);
+         String schoice=new String(dpchoice.getData(),0,dpchoice.getLength());
+            
+        DatagramPacket dp1n=new DatagramPacket(b1num1,b1num1.length);
+        ds.receive(dp1n);
+        String num1=new String(dp1n.getData(),0,dp1n.getLength());
+            
+        DatagramPacket dp2n=new DatagramPacket(b1num2,b1num2.length);
+        ds.receive(dp2n);
+        String num2=new String(dp2n.getData(),0,dp2n.getLength());
+        
+       // System.out.println("str"+str);
+        int num=Integer.parseInt(schoice);
+        int numed1=Integer.parseInt(num1);
+        int numed2=Integer.parseInt(num2);
+        System.out.println("num"+num+"-"+numed1+numed2);
+        int flag=0,result=0;
+        if(num==1)
+        {result=numed1+numed2;
+        flag=+1;}
+        else if(num==2)
+        { result=numed1-numed2;flag=+2;
+            System.out.println("its 2 and reult"+result);
+            System.out.println("its 2 and flag"+flag);
+        System.out.println("its 2 and num"+numed1+numed2);}
+        else if(num==3)
+        { result=numed1*numed2;flag=+3;}
+       else if(num==4)
+        { result=numed1/numed2;flag=+4;}
+       else{System.out.println("Something went wrong ...Please Retry");}
+       // int result=num*num;
+        System.out.println("result"+result);
+         String res = Integer.toString(result); 
+ 
+            //b1ch = res.getBytes(); 
+       byte[] b2=String.valueOf(result).getBytes();
+       byte[] b3=String.valueOf(flag).getBytes();
+       
+        System.out.println("byet"+b2+"**"+flag);
+        InetAddress ia =InetAddress.getLocalHost();
+        DatagramPacket dp1=new DatagramPacket(b2,b2.length,ia,dp1n.getPort());
+        ds.send(dp1);
+        DatagramPacket dpflag=new DatagramPacket(b3,b3.length,ia,dp1n.getPort());
+        ds.send(dpflag);
+        
+    }
+    
+}
